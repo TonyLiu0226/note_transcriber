90054b894344636ab0eead16f83b257468e2881b
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-03-17 09:23:08 -0700

    init

---
+++ code.py
@@ -0,0 +1,70 @@
+# imports for setting up the servo
+from binascii import a2b_base64
+import board
+import pwmio
+from digitalio import DigitalInOut
+import simpleio
+from audiocore import RawSample
+from adafruit_motor import servo
+
+# imports for LCD display
+import terminalio
+import displayio
+from adafruit_display_text import label
+from adafruit_st7735r import ST7735R
+
+# import for setting up the keypad
+import adafruit_matrixkeypad
+
+#additional imports 
+import math
+import array
+import random
+import time
+
+#defines pins for each leg, foot and the speaker
+elbowPin = board.D7
+shoulderPin = board.D5
+wheelPin = board.D2
+
+class PageTurner:
+    def __init__(self):
+        """ constructor for Movement class, initializes the servo objects"""
+        # define PWMOut objects
+        pwmE = pwmio.PWMOut(elbowPin, duty_cycle=2 ** 15, frequency=50)
+        pwmS = pwmio.PWMOut(shoulderPin, duty_cycle=2 ** 15, frequency=50)
+        pwmW = pwmio.PWMOut(wheelPin, frequency=50)
+
+        # define servo objects
+        self.e = servo.Servo(pwmE)
+        self.s = servo.Servo(pwmS)
+        self.w = servo.ContinuousServo(pwmW)
+
+        # reset the angles for each servo
+        self.e.angle = 90
+        self.s.angle = 90
+        time.sleep(1)
+
+    def loop(self):
+        while True:
+            print("spin")
+            self.w.throttle = 1.0
+            time.sleep(0.5)
+            print("stop spin")
+            self.w.throttle = 0.0
+            time.sleep(1.0)
+            print("move shoulder")
+            self.s.angle = 180
+            time.sleep(2.0)
+            print("move elbow")
+            self.e.angle = 140
+            time.sleep(1.0)
+            self.e.angle = 90
+            time.sleep(1.0)
+            self.s.angle = 90
+            time.sleep(1.0)
+
+
+
+if __name__ == "__main__":
+    movement = PageTurner()
