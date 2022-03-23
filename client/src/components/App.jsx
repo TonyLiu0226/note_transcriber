d21d30cfe3635c098b8eef1181ce9fe4735257e8
TonyLiu0226 <TL0226yn@gmail.com>
2022-03-23 00:40:05 -0700

    adds clicker game

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 import { FormControl, Button, TextField } from '@mui/material';
 import './Form.css';
+import { useEffect } from "react";
+
+
+
+function sayHello(){
+  return <div>
+    <h1>"hello"</h1>
+  </div>
+  
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  var text = document.getElementById("bruv");
+  useEffect(() => {
+    document.title = `${count} clicks`
+  }, [count])
+  return <div>
+    <p1>{count}</p1>
+    <button onClick={() => setCount(count + 1) }>
+        Click me lol
+        
+      </button>
+  </div>
+}
 
 export default function App(props) {
-    const [isAuto,setIsAuto] = useState(false);
+  const [isAuto,setIsAuto] = useState(false);
 
-    const toggleAuto = (e) => {
-        e.preventDefault();
-        
-        setIsAuto(prev => !prev);
-    }
+  const toggleAuto = (e) => {
+      e.preventDefault();
+      
+      setIsAuto(prev => !prev);
+  }
 //   const {setToken, isLogin} = props;
 //   const [userInfo, setUserInfo] = useState({
 //     username: "",
@@ -27,7 +52,7 @@ export default function App(props) {
 //       }
 //     });
 //   }
-  
+
 //   function submit(e) {
 //     e.preventDefault();
 //     if (props.isLogin) {
@@ -48,46 +73,50 @@ export default function App(props) {
 //         });
 //     }
 //   }
-  return (
-    <div className="auth-form">
-    <form className="">
-      <FormControl fullWidth={true}>
-      <Button
+return (
+  
+  <div className="auth-form">
+  <form className="">
+    <FormControl fullWidth={true}>
+    <Button
+      variant="contained"
+      component="label"
+      color="secondary"
+      >
+      <input
+          type="file"
+      />
+      </Button>
+      {!isAuto &&
+      <Button 
+        type="submit" 
         variant="contained"
-        component="label"
-        color="secondary"
-        >
-        <input
-            type="file"
-        />
-        </Button>
-        {!isAuto &&
-        <Button 
-          type="submit" 
-          variant="contained" 
-          sx={{my:1}}>Flip Page
-        </Button>
-    }
-        <Button 
-          type="submit" 
-          variant="contained" 
-          sx={{my:1}}
-          onClick={toggleAuto}
-        >Auto
-        </Button>
-        <Button 
-          type="submit" 
-          variant="contained" 
-          sx={{my:1}}>Submit
-        </Button>
-      </FormControl>
-    </form>
-    </div>
+        onClick={sayHello} 
+        sx={{my:1}}>Flip Page
+      </Button>
+  }
+      <Button 
+        type="submit" 
+        variant="contained" 
+        sx={{my:1}}
+        onClick={toggleAuto}
+      >Auto
+      </Button>
+      <Button 
+        type="submit" 
+        variant="contained" 
+        sx={{my:1}}>Submit
+      </Button>
+      
+    </FormControl>
     
-  )
+  </form>
+  <div>
+      <Counter></Counter>
+    </div>
+  </div>
+)
 }
-
-
 /*need to :
 - display preview of scanned pages and offer download option
 
