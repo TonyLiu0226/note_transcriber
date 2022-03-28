96ad303fe2019580b45b297485162008123ac8c8
TonyLiu0226 <TL0226yn@gmail.com>
2022-03-28 11:07:30 -0700

    bruh

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import { Button, ButtonGroup, TextField, AppBar, Toolbar } from 'react-bootstrap';
 import Counter from './Counter';
- 
+import axios from 'axios'; 
 import './Form.css';
 import { useEffect } from "react";
  
- 
- 
+
 export default function App(props) {
  
  
-  const [isAuto,setIsAuto] = useState(false);
+  // const [isAuto,setIsAuto] = useState(false);
  
-  const toggleAuto = (e) => {
-      e.preventDefault();
+  // const toggleAuto = (e) => {
+  //     e.preventDefault();
      
-      setIsAuto(prev => !prev);
-  }
+  //     setIsAuto(prev => !prev);
+  // }
 //   const {setToken, isLogin} = props;
 //   const [userInfo, setUserInfo] = useState({
 //     username: "",
@@ -35,43 +34,55 @@ export default function App(props) {
 //     });
 //   }
  
-//   function submit(e) {
-//     e.preventDefault();
-//     if (props.isLogin) {
-//       axios
-//       .post('http://localhost:4000/api/login', userInfo)
-//       .then((res) => {
-//         return (<Navigate to="/" />)
-//       })
-//       .catch(err => {
-//         console.error(err);
-//       });
-//     } else {
-//       axios
-//         .post('http://localhost:4000/api/register', userInfo)
-//         .then((res) => console.log(res))
-//         .catch(err => {
-//           console.error(err);
-//         });
-//     }
-//   }
+  function submit(e) {
+    e.preventDefault();
+    var data = new FormData();
+    data.append('image', 'cpee.jpg');
+
+    if (props.isLogin) {
+      axios({
+        method: "post",
+        url: "http://cpen291-11.ece.ubc.ca/login",
+        data: data,
+        headers: { "Content-Type": "form-data" },
+      })
+      .then((res) => {
+        return (<p>bruh</p>)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    } else {
+      axios({
+        method: "post",
+        url: "myurl",
+        data: data,
+        headers: { "Content-Type": "form-data" },
+      })
+        .then((res) => console.log(res))
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+
 return (
   <>
   {<div className="auth-form">
-  <div class="container">
+  <div className="container">
   <h2>Actions</h2>
   <br></br>
-  <form class="main" action="/action_page.php" method="post">
-    <div class="form-group">
+  <form className="main" onSubmit={this.submit} method="post">
+    <div className="form-group">
      
-        <button variant="contained" class="btn btn-default btn-lg" component="span" id="bt">TAKE PHOTO
+        <button variant="contained" className="btn btn-default btn-lg" component="span" id="bt">TAKE PHOTO
         <input type="file" accept="image/*" id="file_uploader"/>
         </button>
     </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-default btn-lg" id="bt">Flip page</button>
+    <div className="form-group">
+      <button type="submit" className="btn btn-default btn-lg" id="bt">Flip page</button>
     </div>
-    <button type="submit" class="btn btn-default btn-lg" id="bt">Submit</button>
+    <button type="submit" className="btn btn-default btn-lg" id="bt">Submit</button>
   </form>
 </div>
   <div>
